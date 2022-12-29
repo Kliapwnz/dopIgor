5c74f6d1845e3c8a75b90a8b1a00ae68a8df8f97
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-29 15:58:09 +0300

    add SuperTodolist.tsx

--- src/components/SuperTodolist.tsx
+++ src/components/SuperTodolist.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
-type PropsType={
-    tasks:TasksType[]
-    children?:React.ReactNode
+type PropsType = {
+    tasks: TasksType[]
+    children?: React.ReactNode
 }
 
-type TasksType={
-    id:number
-    title:string
-    isDone:boolean
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
-export const SuperTodolist:React.FC<PropsType> = () => {
+export const SuperTodolist: React.FC<PropsType> = (props) => {
+    const {
+        tasks,
+        children
+    }
+        = props
     return (
         <div>
-            
-            </div>
+
+        </div>
     );
 };
 
