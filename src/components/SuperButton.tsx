0424922f180869dc5a4d2adf4c3f95c0ceb0c12d
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-29 15:50:09 +0300

    secondary

--- src/components/SuperButton.tsx
+++ src/components/SuperButton.tsx
@@ -22,7 +22,7 @@ export const SuperButton: React.FC<PropsType> = (props) => {
 
     // const finalClassName = `${s.button} ${s.default}`
     // const finalClassName = `${s.button} ${color==="red"? s.red : s.default} ${disabled ? s.disabled : ""}`
-    const finalClassName = `${s.button} ${color==="red"? s.red : s.default} ${disabled ? s.disabled : ""}`
+    const finalClassName = `${s.button} ${color==="red"? s.red : color==="secondary" ? s.secondary :s.default} ${disabled ? s.disabled : ""}`
 
     return (
         <button onClick={onClickHandler} className={finalClassName}>{children}</button>
