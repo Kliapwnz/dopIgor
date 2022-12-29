ff1436e87fa6dc623b3d4dc25fbe4012d50feed7
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-29 15:53:44 +0300

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import {SuperButton} from "./components/SuperButton";
 
 
 function App() {
+
+    const tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+
+
     return (
         <div>
             <SuperButton callBack={()=>{}} color={"red"}>Red SuperButton</SuperButton>
